bfe0fbe9f15b10e3b6a9e39ffc8862b46965126e
Itz Merml <jeremy-andreas.schierling@stud.uni-bamberg.de>
2025-01-10 16:03:41 +0100

    Create sqlite database

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import os
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.abspath('db.db')}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+# Klasse Message zur Speicherung von Nachrichten
+class Message(db.Model): 
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.String(200), nullable = True)
+    content = db.Column(db.String(500), nullable = True)
+    created_at = db.Column(db.DateTime, default = datetime.now)
+    
 @app.route('/')
 def start_page():
     return 'Hello, World!'
